ae0d031d7e31c0607852f69587b1815731e96ac5
ugurcant <ugurcan.tuncdoken@ozu.edu.tr>
2015-12-27 12:48:45 +0200

    Deleting Old Files

--- OfiseSoyleExp2/app/src/main/java/ofisesoyle_moduls/AllLists.java
+++
@@ -1,9 +0,0 @@
-package ofisesoyle_moduls;
-
-import java.util.ArrayList;
-
-public class AllLists {
-    public ArrayList<Receivable> productShoppingList = new ArrayList<Receivable>();
-    public ArrayList<Product> productBasketList = new ArrayList<Product>();
-    public ArrayList<Order> productOrderList = new ArrayList<Order>();
-}
